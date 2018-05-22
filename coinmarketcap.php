df82a00cd4fd1a47a5970023cd2c3d619f54e2fb
guiltylotus <minh02091997@gmail.com>
2018-05-22 12:06:16 +0700

    coinmarketcap2

--- coinmarketcap.php
+++ coinmarketcap.php
@@ -25,41 +25,62 @@
 
         button {
             /* background-color: blue; */
+            
         }
     </style>
 
     <!-- js -->
     <script>
         $(document).ready(function(){
+            var countPage = 0;
+            loadCoin("https://api.coinmarketcap.com/v2/ticker/?start=0&limit=10");
+            
+
             $("#btnShow").click(function(){
                 loadCoin();
             });
             
-            function loadCoin(){
-                $.get("https://api.coinmarketcap.com/v2/ticker/",function(data, status) {
-                    // listCoin = JSON.parse(data);
-                    // console.log(data["data"][1]["quotes"]["USD"]["price"]);
+            $("#btn-next").click(function(){
+                countPage += 10;
+                if (countPage > 100) countPage = 100;
+                var url = "https://api.coinmarketcap.com/v2/ticker/?start=" + countPage + "&limit=10"
+                loadCoin(url);
+            });
+            
+            $("#btn-pre").click(function(){
+                countPage -= 10;
+                if (countPage < 0) countPage = 0;
+
+                var url = "https://api.coinmarketcap.com/v2/ticker/?start=" + countPage + "&limit=10"
+                loadCoin(url);
+            });
+            
+
+            function loadCoin(url){
+                
+                $.get(url,function(data, status) {
                     listCoin = data;
 
                     var txt = "";
-                    var count = 0;
+                    var count = countPage;
                     for (i in data["data"]) {
-                        var obj = data["data"][i];
-                        var obj_USD = data["data"][i]["quotes"]["USD"];
-                        
-                        count += 1;
-                        txt += "<tr>";
-                        txt += "<td>" + count + "</td>";
-                        txt += "<td>" + obj["name"] + "</td>";
-                        txt += "<td>" + obj_USD["market_cap"] + "</td>";
-                        txt += "<td>" + obj_USD["price"] + "</td>";
-                        txt += "<td>" + obj_USD["volume_24h"] + "</td>";
-                        txt += "<td>" + obj["circulating_supply"] + "</td>";
-                        txt += "<td>" + obj_USD["percent_change_24h"] + "</td>";
-                        txt += "<td><button class='btn btn-block' id=" + obj["id"] + "></button></td>"
+                            console.log(i);
+                            var obj = data["data"][i];
+                            var obj_USD = data["data"][i]["quotes"]["USD"];
+                            
+                            count += 1;
+                            txt += "<tr>";
+                            txt += "<td>" + count + "</td>";
+                            txt += "<td>" + obj["name"] + "</td>";
+                            txt += "<td>" + obj_USD["market_cap"] + "</td>";
+                            txt += "<td>" + obj_USD["price"] + "</td>";
+                            txt += "<td>" + obj_USD["volume_24h"] + "</td>";
+                            txt += "<td>" + obj["circulating_supply"] + "</td>";
+                            txt += "<td>" + obj_USD["percent_change_24h"] + "</td>";
+                            txt += "<td><button class='btn btn-block' id=" + obj["id"] + "></button></td>"
                         txt += "</tr>";
                     }   
-                    
+                
                     console.log(txt);
                     $("#tbody-coin").html(txt);
                 });
@@ -70,7 +91,17 @@
 
 <body class="container-fluid">
     <h4>CoinMarketCap</h4>
-    <button class="btn btn-block btn-primary" id="btnShow">Show Top Coin</button>
+    <!-- <button class="btn btn-block btn-primary" id="btnShow">Show Top Coin</button> -->
+    <div class="row">
+        <div class="col-md-1">
+            <button class="btn btn-block btn-primary" id="btn-pre">Previous</button>
+        </div> 
+        <div class="col-md-1">
+            <button class="btn btn-block btn-primary" id="btn-next">Next</button>
+        </div> 
+    </div>
+
+   
     <table id="table-coin">
         <tr>
             <th>#</th>
