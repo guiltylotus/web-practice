31da9c0b53ff55f942f7c1b1b69f614e9b5fc137
Nguyen Minh Duc (VTI.Intern) <minh.nguyenduc@vti.vn>
2018-05-30 16:14:40 +0700

    news-project

---
+++ news-project/src/components/News.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="row">
+        <div class="row col-md-8">
+            <div class="col-md-4 box1">
+                
+            </div>
+            <div class="col-md-4 box1">
+                
+            </div>
+            <div class="col-md-4 box1">
+                
+            </div>
+            <div class="col-md-4 box1">
+                
+            </div>
+            <div class="col-md-4 box1">
+                
+            </div>
+            <div class="col-md-4 box1">
+                
+            </div>
+        </div>    
+        <div class="col-md-4">
+            <div class="box2"> {{news_name}}</div>
+            <div class="box2"></div>
+            <div class="box2"></div>
+            <div class="box2"></div>
+            <div class="box2"></div>
+        </div>
+    </div>
+    
+</template>
+
+<script>
+    export default {
+        name: 'news',
+        data() {
+            return {
+                news_name: [],
+            }
+        },
+
+        created() {
+            this.$http.get('../assets/news_name.txt').then(function(response) {
+                console.log(response);
+            });
+        }
+    }
+</script>
+
+<style>
+    .box1 {
+        /* width: 100%; */
+        height: 200px;
+        margin: 20px;
+        border: 2px solid red;
+    }
+
+    .box2 {
+        height: 200px;
+        border: 2px solid green;
+        margin: 20px;
+    }
+</style>
