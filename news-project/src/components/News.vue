384eb600146d2f86baf256f5b250d6ba203ee3e1
Nguyen Duc Minh (VTI.Intern) <minh.nguyenduc@vti.vn>
2018-05-31 12:39:22 +0700

    get api done

--- news-project/src/components/News.vue
+++ news-project/src/components/News.vue
@@ -1,31 +1,18 @@
 <template>
     <div class="row">
-        <div class="row col-md-8">
-            <div class="col-md-4 box1">
-                
+        <div class="col-md-8">
+
+            <div class="col-md-4 box1 " v-for="news in list_news">
+                <div class="avt"></div>
+                <a  v-bind:href="news.url">{{news.title}}</a> 
             </div>
-            <div class="col-md-4 box1">
-                
-            </div>
-            <div class="col-md-4 box1">
-                
-            </div>
-            <div class="col-md-4 box1">
-                
-            </div>
-            <div class="col-md-4 box1">
-                
-            </div>
-            <div class="col-md-4 box1">
-                
-            </div>
+        
         </div>    
-        <div class="col-md-4">
-            <!-- <div class="box2" v-for"new in newsName"> {{new}}</div>             -->
-            <div class="box2">{{getNewsName()}}</div>
-            <div class="box2" v-for="news in newsName">{{news}}</div>
-            <div class="box2"></div>
-            <div class="box2"></div>
+        <div class="col-md-4">           
+            <div class="box2" v-for="news in newsSource">
+                <button class="btn btn-info nbutton" v-bind:id="news" v-on:click="getApi">{{news}}</button>
+            </div>
+            
         </div>
     </div>
     
@@ -37,25 +24,46 @@
         name: 'news',
         data() {
             return {
-                newsName: data_news.news,
+                list_news: [],
+                newsSource: data_news.news,
+                amount_news: 100,
+                
             }
         },
 
-        created() {
-            // this.$http.get('../assets/news_name.txt').then(function(response) {
-            //     console.log(response);
-            // });
-            console.log(data_news);
+        mounted() {
+            let self = this
+            this.$http.get('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=714b3a33941d45b58e2e57d11ee38d7c')
+            .then(function(response) {
+                var count = 0;
+                // console.log(response);
+                for (let rdata in response.data.articles) 
+                    if (count < self.amount_news) {
+                        count++;
+                        self.list_news.push(response.data.articles[rdata]);
+                    }
+                
+                // console.log(self.list_news);
+            })
+            
         },
 
         methods: {
-            getNewsName() {
-                var list = [];
-                for (let i in this.newsName) {
-                    list.push(i);
-                }
-                console.log(list);
-                return "hello";
+            getApi(event) {
+                var self = this;
+                var id = event.target.id;
+                var url = "https://newsapi.org/v2/everything?sources=" + id + "&apiKey=714b3a33941d45b58e2e57d11ee38d7c";
+                this.$http.get(url).then(function(response) {
+                    var count = 0;
+                    self.list_news = [];
+                    for (let rdata in response.data.articles)
+                        if (count < self.amount_news) {
+                            count++;
+                            self.list_news.push(response.data.articles[rdata]);
+                        }
+                    // console.log(event);
+                    console.log(self.list_news);
+                })
             }
         }
     }
@@ -64,14 +72,27 @@
 <style>
     .box1 {
         /* width: 100%; */
-        height: 200px;
+        height: 250px;
         margin: 20px;
         border: 2px solid red;
     }
 
+    .avt {
+        height: 150px;
+        background-color: bisque;
+    }
+
     .box2 {
-        height: 200px;
-        border: 2px solid green;
+        /* background-color: gray;
+        width: 50%;
+        height: 50px;
+        line-height: 50px;
+        border: 2px solid green; */
         margin: 20px;
+    }
+
+    .nbutton {
+        width: 70%;
+        height: 50px;
     }
 </style>
